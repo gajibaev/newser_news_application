c7999cbd92b1006cb69ed3de7c4ce2fd5245a06c
Alibek.G <agadzhibaev@gmail.com>
2022-07-30 06:10:57 +0300

    add key api changed field in file

---
+++ lib/api_key.dart
@@ -0,0 +1,5 @@
+// You can get own api key from:
+// https://developer.nytimes.com/apis (Times Newswire API)
+// Only for personal use
+
+const String apiKey = 'IfIkjNXQBaPVclsYanpWLHGXYXT6OpL0';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:news_application/api_key.dart';
 import 'package:news_application/app.dart';
 import 'package:news_repository/news_repository.dart';
 
 void main() {
-  runApp(App(newsRepository: NewsRepository()));
+  runApp(App(newsRepository: NewsRepository(apiKey)));
 }
+
+// TODO Добавить форматирование даты
